fcc842421a0d55d3106fbcfe252c4b8a61fb31d4
SUNDAR <aaransoftware@gmail.com>
2025-03-26 16:01:28 +0530

    #2 fixing

--- aaran/Website/Routes/web.php
+++ aaran/Website/Routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['website'])->group(function () {
 
-    Route::get('/ff', \Aaran\Website\Livewire\Class\Index::class)->name('home');
+    Route::get('/', \Aaran\Website\Livewire\Class\Index::class)->name('home');
 
     Route::get('/about', \Aaran\Website\Livewire\Class\About::class)->name('about');
 });
